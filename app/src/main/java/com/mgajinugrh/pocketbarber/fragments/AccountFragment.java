dd8eab50ac743ee50f9cbf53a50ce12edc9b1b9e
MGAjinugrh <mg.ajinugroho@gmail.com>
2019-09-11 13:34:19 +0700

    Splash Screen

--- app/src/main/java/com/mgajinugrh/pocketbarber/fragments/AccountFragment.java
+++ app/src/main/java/com/mgajinugrh/pocketbarber/fragments/AccountFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import com.mgajinugrh.pocketbarber.R;
 
@@ -16,5 +17,6 @@ public class AccountFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_account, container, false);
         return view;
+
     }
 }
